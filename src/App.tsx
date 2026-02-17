7ab7552711cf114c8ea3b58b5cbe61361d7685fd
Atharva Taware <atharvatware57@gmail.com>
2026-02-17 16:04:23 +0530

    Subproducts

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Locations from './pages/Store';
 import Products from './pages/products';
-import Header from './components/Header';  
-import SubProducts from './pages/SubProducts';
+import Header from './components/Heeader';
+import SubProducts from './pages/SubProducts'; 
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/locations" element={<Locations />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:categorySlug" element={<SubProducts />} />
       </Routes>
-      <Header />
     </Router>
   );
 }
+
